7b8d5b6e84086563d4d41f9e250587cf1483f495
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-07-22 10:14:44 +0200

    update

--- src/Components/LayeredTensorNetwork.jl
+++ src/Components/LayeredTensorNetwork.jl
@@ -1,7 +1,20 @@
 using QuantumTags
+import QuantumTags: layers
+using DelegatorTraits
 
 struct LayeredTensorNetwork <: AbstractTensorNetwork
     tn::GeneralizedTensorNetwork
 end
 
 LayeredTensorNetwork() = LayeredTensorNetwork(GeneralizedTensorNetwork())
+
+ImplementorTrait(interface, tn::MPS) = ImplementorTrait(interface, tn.tn)
+function DelegatorTrait(interface, tn::MPS)
+    if ImplementorTrait(interface, tn.tn) === Implements()
+        DelegateToField{:tn}()
+    else
+        DontDelegate()
+    end
+end
+
+layers(tn::LayeredTensorNetwork) = unique(map(layer, all_sites_iter(tn)))
